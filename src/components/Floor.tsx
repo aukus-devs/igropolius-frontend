b7bc8356f7a70f00bf1e6928081dc5050ea85268
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-04-09 19:34:51 +0200

    Lighten floor color

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -2,14 +2,11 @@ import { FLOOR_HEIGHT, FLOOR_SIZE } from "@/lib/constants";
 
 export function Floor() {
   return (
-    <mesh
-      rotation={[-Math.PI / 2, 0, 0]}
-      receiveShadow
-    >
+    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
       <boxGeometry args={[FLOOR_SIZE, FLOOR_SIZE, FLOOR_HEIGHT]} />
-      <meshStandardMaterial color="#0a0a0a" />
+      <meshStandardMaterial color="#2a2a2a" />
     </mesh>
-  )
+  );
 }
 
 export default Floor;
