c7f8f7586fb621edd935b35f35e66d0b3100a6e1
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-04-09 18:46:32 +0200

    Display sector ID in info box

--- src/App.tsx
+++ src/App.tsx
@@ -280,8 +280,9 @@ function App() {
           <div className="absolute top-10 left-0 z-10 flex items-center justify-center w-full h-16 bg-black bg-opacity-50 backdrop-blur-sm">
             <h1 className="text-white">Event 3D</h1>
           </div>
-          {selectedSector && (
+          {selectedSector && selectedSectorId !== null && (
             <div className="absolute top-16 left-20 z-20 p-4 text-white bg-black bg-opacity-50 backdrop-blur-sm border-2 border-white rounded-lg">
+              <h2 className="text-2xl font-bold">Клетка #{selectedSectorId + 1}</h2>
               <h2>{selectedSector.name}</h2>
               <p>Type: {selectedSector.type}</p>
             </div>
