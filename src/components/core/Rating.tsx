3582efddc6d875cbaede86697c9231c3c3980854
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-04-16 11:50:29 +0200

    Adjust rating color scale

--- src/components/core/Rating.tsx
+++ src/components/core/Rating.tsx
@@ -3,16 +3,17 @@ import { Button } from "../ui/button";
 
 type Props = {
   onChange?: (value: number) => void;
-}
+};
 
 function Rating({ onChange }: Props) {
   const [value, setValue] = useState(0);
   const [hoverValue, setHoverValue] = useState<number | null>(null);
   const [hoverPosition, setHoverPosition] = useState<number | null>(null);
-  let color = "bg-green-500";
+  let color = "bg-purple-500";
 
-  if ((hoverValue || value) < 4) color = "bg-red-500"
+  if ((hoverValue || value) < 4) color = "bg-red-500";
   else if ((hoverValue || value) < 7) color = "bg-yellow-500";
+  else if ((hoverValue || value) < 9) color = "bg-green-500";
 
   const renderSector = (index: number) => {
     const ratingValue = index + 1;
@@ -25,14 +26,16 @@ function Rating({ onChange }: Props) {
       <div
         className={`relative bg-muted-foreground transition-all rounded-xl h-full w-11 hover:scale-115 overflow-hidden active:scale-100 ${isActive ? "scale-115" : ""} overflow-hidden`}
       >
-        <div className="relative z-20 pointer-events-none w-full h-full flex items-center justify-center">{ratingValue}</div>
+        <div className="relative z-20 pointer-events-none w-full h-full flex items-center justify-center">
+          {ratingValue}
+        </div>
         <div
           className={`absolute left-0 top-0 z-10 w-full h-full ${color} rounded-none transition-colors`}
           style={{ width: isFull ? "100%" : isHalf ? "50%" : "0%" }}
         />
       </div>
-    )
-  }
+    );
+  };
 
   function handleClick(index: number) {
     const newValue = hoverPosition && hoverPosition > 0.5 ? index + 1 : index + 0.5;
@@ -64,10 +67,10 @@ function Rating({ onChange }: Props) {
           >
             {renderSector(index)}
           </Button>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default Rating;
