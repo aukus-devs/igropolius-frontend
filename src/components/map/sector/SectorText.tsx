c2d4791f26e10d613da36f4ee56b45e3a6a8f288
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-04-19 00:15:51 +0200

    Adjust sector text position

--- src/components/map/sector/SectorText.tsx
+++ src/components/map/sector/SectorText.tsx
@@ -4,22 +4,16 @@ import { Text } from "@react-three/drei";
 type Props = {
   text: string;
   isCorner: boolean;
-}
+};
 
 function SectorText({ text, isCorner }: Props) {
-  const position: Vector3Array = isCorner
-    ? [-5, 0.1, -5]
-    : [0, 0.1, -5];
+  const position: Vector3Array = isCorner ? [-4.5, 0.1, -4.5] : [0, 0.1, -5];
   const rotation: Vector3Array = isCorner
     ? [Math.PI / 2, Math.PI, Math.PI / 4]
     : [Math.PI / 2, Math.PI, 0];
 
   return (
-    <Text
-      position={position}
-      rotation={rotation}
-      color="black"
-    >
+    <Text position={position} rotation={rotation} color="black">
       {text}
     </Text>
   );
