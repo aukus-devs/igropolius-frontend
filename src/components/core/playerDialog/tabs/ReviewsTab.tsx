7b4fb87f493d3e3e2f050c51a179d6737fa47458
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-04-21 12:30:38 +0200

    Add MursGothic Wide Medium font
    Apply the new font to the search input field in the reviews tab.

--- src/components/core/playerDialog/tabs/ReviewsTab.tsx
+++ src/components/core/playerDialog/tabs/ReviewsTab.tsx
@@ -6,30 +6,37 @@ import GameReview from "../../GameReview";
 
 type Props = {
   reviews: GameReviewType[];
-}
+};
 
 function ReviewsTab({ reviews }: Props) {
   const [searchText, setSearchText] = useState("");
-  const filteredReviews = reviews.filter((review) => review.gameTitle.toLowerCase().includes(searchText.toLowerCase()));
+  const filteredReviews = reviews.filter((review) =>
+    review.gameTitle.toLowerCase().includes(searchText.toLowerCase()),
+  );
 
   return (
     <>
       <div className="relative">
-        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground" size="1rem" />
+        <SearchIcon
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+          size="1rem"
+        />
         <Input
           id="search"
           type="text"
-          className="pl-8"
+          className="pl-8 font-wide-medium"
           placeholder="Поиск по играм"
           onKeyDown={(e) => e.stopPropagation()}
           onChange={(e) => setSearchText(e.target.value)}
         />
       </div>
       <div className="flex flex-col gap-8 py-8">
-        {filteredReviews.map((review) => <GameReview key={review.gameTitle} review={review} />)}
+        {filteredReviews.map((review) => (
+          <GameReview key={review.gameTitle} review={review} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
 export default ReviewsTab;
