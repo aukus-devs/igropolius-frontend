c00502b1070da697d06aa5fc9201ade64d6d1e69
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-06-08 18:32:55 +0200

    Remove unused pointer-events-auto class from LoadingModal

--- src/components/core/LoadingModal.tsx
+++ src/components/core/LoadingModal.tsx
@@ -1,8 +1,7 @@
 export default function LoadingModal() {
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50
-                   pointer-events-auto"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
       aria-modal="true"
       role="dialog"
       aria-label="Loading"
