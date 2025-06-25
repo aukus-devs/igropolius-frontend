711309f7e9b20fbd9095e33263e2104d5808bbf5
roman <roma.preslitsky@gmail.com>
2025-06-25 05:45:18 +0300

    New Roulette

--- src/components/core/turnUI/RollBonusCard.tsx
+++ src/components/core/turnUI/RollBonusCard.tsx
@@ -1,138 +1,275 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "../../ui/dialog";
 import { Button } from "../../ui/button";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { frontendCardsData } from "@/lib/mockData";
 import { Card, CardFooter } from "../../ui/card";
 import { BonusCardType } from "@/lib/types";
 import usePlayerStore from "@/stores/playerStore";
 import { useShallow } from "zustand/shallow";
 
-const ROLLING_TIME_MS = 9000;
+const BACKSWING_DURATION = 500;
+const SPIN_DURATION = 8000;
+const SETTLE_DURATION = 300;
+const CARD_WIDTH = 160;
+const CARD_HEIGHT = Math.floor(CARD_WIDTH * 1.4);
+const GAP = 8;
+const CARD_FULL_WIDTH = CARD_WIDTH + GAP;
+const IDLE_CARD_COUNT = 21;
 
 export default function RollBonusCard() {
   const rouletteRef = useRef<HTMLDivElement>(null);
-  const rouletteContainerRef = useRef<HTMLDivElement>(null);
-  const [open, setOpen] = useState(false);
-  const [state, setState] = useState<"idle" | "rolling" | "finished">("idle");
-  const [displayedCards, setDisplayedCards] = useState<BonusCardType[]>([]);
-  const [winnerIndex, setWinnerIndex] = useState<number | null>(null);
+  const animationRef = useRef<number | null>(null);
+  const centerIndexRef = useRef(0);
+  const offsetRef = useRef(0);
+  const isIdleRunningRef = useRef(false);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [rollState, setRollState] = useState<"idle" | "rolling" | "finished">(
+    "idle"
+  );
+  const [cardList, setCardList] = useState<BonusCardType[]>([]);
+  const [headerText, setHeaderText] = useState("Роллим бонусную карточку");
   const { receiveBonusCard, myPlayer } = usePlayerStore(
     useShallow((state) => ({
       receiveBonusCard: state.receiveBonusCard,
       myPlayer: state.myPlayer,
-    })),
+    }))
   );
 
   const myCurrentCards = myPlayer?.bonus_cards ?? [];
-  const myCurrentCardsTypes = myCurrentCards.map((card) => card.bonus_type) as BonusCardType[];
+  const myCurrentCardsTypes = myCurrentCards.map(
+    (card) => card.bonus_type
+  ) as BonusCardType[];
 
   const allCardTypes = Object.keys(frontendCardsData) as BonusCardType[];
   const cardTypesForRoll = allCardTypes.filter(
-    (cardType) => !myCurrentCardsTypes.includes(cardType),
+    (cardType) => !myCurrentCardsTypes.includes(cardType)
   );
 
-  const winner = winnerIndex ? displayedCards[winnerIndex] : null;
+  function updateTransform() {
+    if (rouletteRef.current) {
+      rouletteRef.current.style.transform = `translateX(${offsetRef.current}px)`;
+    }
+  }
 
-  const cardWidth = 80;
-  const cardHeight = Math.floor(cardWidth * 1.4);
-  const gap = 8;
+  const idleAnimate = () => {
+    if (rollState !== "idle") return;
+
+    const speed = 1;
+    const direction = -1;
+    offsetRef.current = offsetRef.current + speed * direction;
+
+    updateTransform();
+
+    if (
+      -Math.floor(offsetRef.current / CARD_FULL_WIDTH) !==
+      centerIndexRef.current
+    ) {
+      if (direction < 0) {
+        setCardList((prev) => [
+          ...prev.slice(1),
+          getRandomExcept(cardTypesForRoll, prev[prev.length - 1]),
+        ]);
+        offsetRef.current += CARD_FULL_WIDTH;
+      } else {
+        setCardList((prev) => [
+          getRandomExcept(cardTypesForRoll, prev[0]),
+          ...prev.slice(0, -1),
+        ]);
+        offsetRef.current -= CARD_FULL_WIDTH;
+      }
+    }
+
+    animationRef.current = requestAnimationFrame(idleAnimate);
+  };
+
+  function easeBackswing(t: number): number {
+    //TODO
+    return 1 - Math.pow(1 - t, 3);
+  }
+
+  function easeSpin(t: number): number {
+    //TODO
+    return 1 - Math.pow(1 - t, 3);
+  }
+
+  function startRollingAnimation(callback?: () => void) {
+    if (rollState !== "rolling") return;
+
+    var startOffset = offsetRef.current;
+    const backswingOffset = startOffset + 30;
+
+    const posInsideCard = backswingOffset % CARD_FULL_WIDTH;
+    const randomOffset = Math.floor(Math.random() * CARD_FULL_WIDTH);
+
+    // const shouldBounce = randomOffset > CARD_WIDTH;
+
+    const endOffset =
+      backswingOffset - posInsideCard - CARD_FULL_WIDTH * 150 - randomOffset;
+
+    let phase: "backswing" | "spin" | "settle" = "backswing";
+    let animationStart = 0;
+
+    function animate(now: number) {
+      if (animationStart === 0) {
+        animationStart = now;
+      }
+
+      const elapsed = now - animationStart;
+
+      if (phase === "backswing") {
+        const progress = Math.min(elapsed / BACKSWING_DURATION, 1);
+        const eased = easeBackswing(progress);
+
+        offsetRef.current =
+          startOffset + (backswingOffset - startOffset) * eased;
+
+        updateTransform();
+
+        if (progress >= 1) {
+          phase = "spin";
+          animationStart = now;
+        }
+        animationRef.current = requestAnimationFrame(animate);
+      } else if (phase === "spin") {
+        const progress = Math.min(elapsed / SPIN_DURATION, 1);
+        const eased = easeSpin(progress);
+
+        offsetRef.current = startOffset + (endOffset - startOffset) * eased;
+
+        updateTransform();
+
+        if (progress >= 1) {
+          phase = "settle";
+          animationStart = now;
+        }
+        animationRef.current = requestAnimationFrame(animate);
+      } else if (phase === "settle") {
+        const winnerIndex = Math.floor(-offsetRef.current / CARD_FULL_WIDTH);
+        const winnerCard = cardList[winnerIndex];
+        const cardInfo = frontendCardsData[winnerCard];
+        setHeaderText(`Карточка «${cardInfo.name}»: ${cardInfo.description}`);
+        setRollState("finished");
+        animationRef.current = null;
+      }
+    }
+    animationRef.current = requestAnimationFrame(animate);
+  }
 
   const handleOpenChange = (isOpen: boolean) => {
     if (isOpen) {
-      const randomCards = generateList(80, cardTypesForRoll);
-      setDisplayedCards(randomCards);
-      setState("idle");
-      setWinnerIndex(null);
+      const randomCards = generateList(IDLE_CARD_COUNT, cardTypesForRoll);
+      setCardList(randomCards);
+      setRollState("idle");
+      centerIndexRef.current = Math.floor(IDLE_CARD_COUNT / 2);
+      offsetRef.current = -(
+        (centerIndexRef.current - 1) * CARD_FULL_WIDTH +
+        CARD_WIDTH / 2
+      );
+      setHeaderText("Роллим бонусную карточку");
+    } else {
+      if (animationRef.current !== null) {
+        cancelAnimationFrame(animationRef.current);
+      }
+      animationRef.current = null;
+      isIdleRunningRef.current = false;
     }
-    setOpen(isOpen);
+    setIsOpen(isOpen);
   };
 
+  useEffect(() => {
+    if (rollState === "idle" && cardList.length > 0) {
+      if (!isIdleRunningRef.current) {
+        isIdleRunningRef.current = true;
+        animationRef.current = requestAnimationFrame(idleAnimate);
+      }
+    }
+    if (rollState === "rolling" && cardList.length > IDLE_CARD_COUNT) {
+      startRollingAnimation();
+    }
+  }, [cardList, rollState]);
+
   const handleRollClick = () => {
-    if (!rouletteRef.current) return;
-    setState("rolling");
+    setRollState("rolling");
 
-    const visibleCards = 9;
-    const totalCards = displayedCards.length;
-    const centerIndex = Math.floor(visibleCards / 2);
+    const randomCards = generateList(150, cardTypesForRoll);
+    setCardList((prev) => [...prev, ...randomCards]);
 
-    const winIndex =
-      Math.floor(Math.random() * visibleCards) + (totalCards - visibleCards * 2);
-    setWinnerIndex(winIndex);
-
-    rouletteRef.current.style.transition = "transform 0.4s cubic-bezier(0,.26,.46,3)";
-    rouletteRef.current.style.transform = `translateX(15px)`;
-
-    setTimeout(() => {
-      if (!rouletteRef.current) return;
-
-      const scrollDistance = (winIndex + 1 - centerIndex) * (cardWidth + gap);
-
-      rouletteRef.current.style.left = `0px`; // No calc() needed
-      rouletteRef.current.style.transition = `transform ${ROLLING_TIME_MS}ms cubic-bezier(0.1,.41,.17,1.0)`;
-      rouletteRef.current.style.transform = `translateX(-${scrollDistance}px)`;
-
-      setTimeout(() => {
-        setState("finished");
-      }, ROLLING_TIME_MS);
-    }, 400);
+    if (animationRef.current !== null) {
+      cancelAnimationFrame(animationRef.current);
+    }
   };
 
   return (
-    <Dialog open={open} onOpenChange={handleOpenChange}>
+    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
       <DialogTrigger asChild>
         <Button variant="outline">Зароллить карточку</Button>
       </DialogTrigger>
-      <DialogContent style={{ minWidth: "600px" }} className="px-0 overflow-hidden">
-        <DialogHeader>
-          <DialogTitle>Роллим бонусную карточку</DialogTitle>
-          <DialogDescription className="sr-only">Ролл карточек</DialogDescription>
-
-          <div
-            ref={rouletteContainerRef}
-            style={{ height: `${cardHeight}px`, justifyContent: "center" }}
-            className="flex items-center"
-          >
-            <div className="absolute left-1/2 top-0 bottom-0 w-[1px] bg-white z-10" />
-            <div ref={rouletteRef} style={{ gap: `${gap}px` }} className="absolute flex">
-              {displayedCards.map((item, idx) => {
-                const cardData = frontendCardsData[item];
-                return (
-                  <Card
-                    key={idx}
-                    style={{ height: `${cardHeight}px`, width: `${cardWidth}px` }}
-                    className="overflow-hidden"
-                  >
-                    <img src={cardData.picture} className="absolute top-0"></img>
-                    <CardFooter className="z-10">{cardData.name}</CardFooter>
-                  </Card>
-                );
-              })}
-            </div>
-            {state === "idle" && (
-              <Button variant="secondary" className="z-20" onClick={handleRollClick}>
-                Gamba
-              </Button>
-            )}
-            {state === "finished" && winner && (
-              <Button
-                variant="secondary"
-                className="z-20"
-                onClick={() => receiveBonusCard(winner)}
-              >
-                Получить карточку: {winner} <br />
-                {frontendCardsData[winner].name}
-              </Button>
-            )}
-          </div>
+      <DialogContent
+        className="flex flex-col items-center justify-center  w-full  min-w-full backdrop-blur-sm h-full bg-transparent shadow-none"
+        onEscapeKeyDown={(e) => {
+          if (rollState === "rolling") {
+            e.preventDefault();
+          }
+        }}
+      >
+        <DialogHeader className="absolute bottom-[70%] max-w-4xl">
+          <DialogTitle className="font-wide-black text-3xl text-center">
+            {headerText}
+          </DialogTitle>
         </DialogHeader>
+        <div className="absolute left-1/2">
+          <div className="absolute z-20 top-0 -translate-x-1/2 -translate-y-1/2 border-l-[18px] border-r-[18px] border-t-[18px] border-l-transparent border-r-transparent border-t-red-500" />
+          <div className="absolute z-20 bottom-0 -translate-x-1/2 translate-y-1/2 border-l-[18px] border-r-[18px] border-b-[18px] border-l-transparent border-r-transparent border-b-red-500" />{" "}
+          <div
+            ref={rouletteRef}
+            style={{ gap: `${GAP}px` }}
+            className=" flex items-center"
+          >
+            {cardList.map((item, idx) => {
+              const cardData = frontendCardsData[item];
+              return (
+                <Card
+                  key={idx}
+                  style={{
+                    height: `${CARD_HEIGHT}px`,
+                    width: `${CARD_WIDTH}px`,
+                    transition: "all 0.3s ease",
+                  }}
+                  className="overflow-hidden shadow-none relative"
+                >
+                  <img src={cardData.picture} className="absolute top-0" />
+                  <CardFooter className="z-10">{item}</CardFooter>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+
+        {rollState === "idle" && (
+          <Button
+            variant="secondary"
+            className="z-20 rounded-xl"
+            onClick={handleRollClick}
+          >
+            Gamba
+          </Button>
+        )}
+
+        <Button
+          variant="secondary"
+          className="absolute w-[300px] rounded-xl bottom-[25%]"
+          onClick={() => handleOpenChange(false)}
+          disabled={rollState === "rolling"}
+        >
+          Закрыть
+        </Button>
       </DialogContent>
     </Dialog>
   );
@@ -148,7 +285,8 @@ function generateList<T>(len: number, options: T[]): T[] {
   const result: T[] = [];
   for (let i = 0; i < len; i++) {
     // Filter out the previous item to avoid duplicates
-    const available = i === 0 ? options : options.filter((opt) => opt !== result[i - 1]);
+    const available =
+      i === 0 ? options : options.filter((opt) => opt !== result[i - 1]);
     if (available.length === 0) {
       throw new Error("No valid options to choose from at position " + i);
     }
@@ -162,3 +300,22 @@ function generateList<T>(len: number, options: T[]): T[] {
 // Example usage:
 // const list = generateList(10, ['A', 'B', 'C']);
 // console.log(list);
+
+function getRandomExcept<T>(options: T[], exclude: T): T {
+  if (options.length === 0) {
+    throw new Error("Need at least 1 option");
+  }
+
+  const available = options.filter((opt) => exclude !== opt);
+
+  if (available.length === 0) {
+    throw new Error("No valid options to choose after excluding");
+  }
+
+  return available[Math.floor(Math.random() * available.length)];
+}
+
+// function getInitialCardCount(): number {
+//   let n = Math.ceil(window.innerWidth / (CARD_WIDTH + GAP));
+//   return n;
+// }
