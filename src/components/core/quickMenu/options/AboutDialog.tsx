8d79ee9cdb2e0b60fa5698c32db3a7a0778ad42c
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-07-02 01:29:41 +0200

    Add ScrollArea to AboutDialog and expand event info content

--- src/components/core/quickMenu/options/AboutDialog.tsx
+++ src/components/core/quickMenu/options/AboutDialog.tsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
-import { buttonVariants } from "../../../ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../../../ui/dialog";
-import { InfoIcon } from "lucide-react";
-import { cn } from "@/lib/utils";
-import useLocalStorage from "@/hooks/useLocalStorage";
+import { useEffect, useState } from 'react';
+import { buttonVariants } from '../../../ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../../../ui/dialog';
+import { InfoIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 type Props = {
   className?: string;
@@ -11,7 +18,7 @@ type Props = {
 
 export default function AboutDialog({ className }: Props) {
   const { value: firstTimeVisit, save: saveFirstTimeVisit } = useLocalStorage({
-    key: "first-time-visit",
+    key: 'first-time-visit',
     defaultValue: true,
   });
   const [isOpen, setIsOpen] = useState(firstTimeVisit ? true : false);
@@ -22,23 +29,31 @@ export default function AboutDialog({ className }: Props) {
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger
-        className={cn(buttonVariants({ variant: "outline" }), className)}
-      >
+      <DialogTrigger className={cn(buttonVariants({ variant: 'outline' }), className)}>
         <InfoIcon />
         Об ивенте
       </DialogTrigger>
       <DialogContent className="w-[37.5rem]" aria-describedby="">
-        <DialogHeader>
-          <DialogTitle className="text-2xl font-bold text-center">Об ивенте</DialogTitle>
-        </DialogHeader>
-        Игрополис — это увлекательный ивент, где популярные стримеры соревнуются друг с другом,
-        проходя различные игры. Каждый участник бросает кубик, чтобы сделать ход по специальной
-        игровой карте, выполняет задания и стремится первым достичь финиша.
-        <br />
-        <br />
-        Побеждает тот, кто проявит ловкость, смекалку и удачу, чтобы обойти соперников и стать
-        чемпионом Игрополиса!
+        <ScrollArea className="overflow-auto h-[30rem]">
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-bold text-center">Об ивенте</DialogTitle>
+          </DialogHeader>
+          Игрополис — это увлекательный ивент, где популярные стримеры соревнуются друг с другом,
+          проходя различные игры. Каждый участник бросает кубик, чтобы сделать ход по специальной
+          игровой карте, выполняет задания и стремится первым достичь финиша.
+          <br />
+          <br />
+          Побеждает тот, кто проявит ловкость, смекалку и удачу, чтобы обойти соперников и стать
+          чемпионом Игрополиса!
+          <div className="mt-20 mb-[30px] text-3xl font-wide-semibold">
+            Команда разработки Игрополиуса
+          </div>
+          <div className="mt-[50px] text-xl font-wide-semibold">Программирование</div>
+          <div className="mt-[50px] text-xl font-wide-semibold">Дизайн</div>
+          <div className="mt-[50px] text-xl font-wide-semibold">Тестирование</div>
+          <div className="mt-[50px] text-xl font-wide-semibold">Идеи</div>
+          <div className="mt-[10px]">Praden</div>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
