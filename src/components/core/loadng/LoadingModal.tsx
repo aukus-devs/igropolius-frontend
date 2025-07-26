f5006b46e9aab4b9a4a5a16b2aa32c2e907f00fe
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-07-27 00:01:06 +0200

    Center loading modal content vertically

--- src/components/core/loadng/LoadingModal.tsx
+++ src/components/core/loadng/LoadingModal.tsx
@@ -3,13 +3,13 @@ import { LoaderCircleIcon } from 'lucide-react';
 export default function LoadingModal() {
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50"
       aria-modal="true"
       role="dialog"
       aria-label="Loading"
     >
-      <p>Подключаюсь к серверам...</p>
-      <LoaderCircleIcon className="animate-spin text-primary" />
+      <div>Подключаюсь к серверам</div>
+      <LoaderCircleIcon className="animate-spin text-primary" size={50} />
     </div>
   );
 }
