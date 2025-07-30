17898d26e629a9b83de1ef696b9d88516860c05d
olegsvs <oleg.texet@gmail.com>
2025-07-30 19:49:36 +0300

    Update page on login

--- src/components/core/quickMenu/options/LoginDialog.tsx
+++ src/components/core/quickMenu/options/LoginDialog.tsx
@@ -33,6 +33,7 @@ export default function LoginDialog({ className }: { className?: string }) {
         localStorage.setItem('access-token', res.token);
         resetCurrentPlayerQuery();
         setOpen(false);
+        window.location.reload();
       })
       .catch(err => {
         if (err.body) {
