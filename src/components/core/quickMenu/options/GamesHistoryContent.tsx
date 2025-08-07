40961952b2c091840ba60ad89f8f05189d28bf47
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-08-07 19:21:28 +0200

    Feature: Add game history filtering and display
    Adds: - Input for filtering game history by title, nickname, event, or review - Display of game
    cover, time, rating, and review - Utility function to convert seconds to hours and minutes
    
    Improves the display of game history with filtering and more details.

--- src/components/core/quickMenu/options/GamesHistoryContent.tsx
+++ src/components/core/quickMenu/options/GamesHistoryContent.tsx
@@ -1,5 +1,9 @@
+import { Badge } from '@/components/ui/badge';
+import { Input } from '@/components/ui/input';
+import { useDebounce } from '@/hooks/useDebounce';
 import { useQuery } from '@tanstack/react-query';
 import { LoaderCircleIcon } from 'lucide-react';
+import { useMemo, useState } from 'react';
 
 type Historyitem = {
   player_nickname: string;
@@ -27,6 +31,23 @@ export default function GamesHistoryContent() {
     },
   });
 
+  const [searchFilter, setSearchFilter] = useState('');
+
+  const debouncedFilter = useDebounce(searchFilter.toLowerCase(), 300);
+
+  const filteredGames = useMemo(() => {
+    if (!historyData?.games.length) {
+      return [];
+    }
+    return historyData.games.filter(
+      item =>
+        item.game_title.toLowerCase().includes(debouncedFilter) ||
+        item.player_nickname.toLowerCase().includes(debouncedFilter) ||
+        item.event_name.toLowerCase().includes(debouncedFilter) ||
+        item.review.toLowerCase().includes(debouncedFilter)
+    );
+  }, [debouncedFilter, historyData?.games.length]);
+
   if (isLoading || !historyData) {
     return (
       <div className="flex justify-center mt-20">
@@ -36,27 +57,53 @@ export default function GamesHistoryContent() {
   }
 
   return (
-    <div className="flex flex-col gap-10 w-full">
-      {historyData.games.map((item, idx) => (
-        <div key={idx} className="w-full min-w-0 flex-1">
-          <div>
-            {item.player_nickname}&nbsp;—&nbsp;{item.event_name}&nbsp;—&nbsp;
-            {item.completion_status}
-          </div>
-          <div className="text-2xl">{item.game_title}</div>
-          <div className="flex gap-2">
-            <div className="min-w-[150px]">
-              <img src={item.game_cover} className="min-w-[150px] w-[150px]"></img>
+    <div>
+      <Input
+        className="mt-[30px] mb-[30px] font-roboto-wide-semibold bg-foreground/10 border-none"
+        type="text"
+        value={searchFilter}
+        onChange={e => setSearchFilter(e.target.value)}
+        placeholder="Поиск"
+        onKeyDown={e => e.stopPropagation()}
+      />
+      <div className="flex flex-col gap-[50px] w-full">
+        {filteredGames.map(item => (
+          <div
+            key={`${item.player_nickname}-${item.game_title}-${item.date}`}
+            className="flex flex-col gap-[10px]"
+          >
+            <div className="font-roboto-wide-semibold">
+              {item.player_nickname}&nbsp;—&nbsp;{item.event_name}&nbsp;—&nbsp;
+              {item.completion_status}
             </div>
-            <div className="">
-              <div className="flex gap-2">
-                <div>Время: {item.game_time}сек.</div>
+            <div className="text-2xl p-0 font-roboto-wide-semibold">{item.game_title}</div>
+            <div className="flex gap-[8px]">
+              <div className="min-w-[150px]">
+                {item.game_cover && (
+                  <img src={item.game_cover} className="min-w-[150px] w-[150px]"></img>
+                )}
               </div>
-              <span className="break-words whitespace-normal text-pretty">{item.review}</span>
+              <div className="flex flex-col gap-[10px]">
+                <div className="flex gap-2">
+                  <Badge className="bg-white/20 text-white/70 font-semibold">
+                    Пройдено за: {sencondsToHourMin(item.game_time)}
+                  </Badge>
+                </div>
+                <span className="break-words whitespace-normal text-pretty">
+                  {item.rating && <span>{item.rating}&nbsp;—&nbsp;</span>}
+                  {item.review}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
+}
+
+function sencondsToHourMin(seconds: number): string {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  return `${hours}ч ${minutes}мин`;
 }
