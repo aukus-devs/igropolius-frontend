5666fbca1ba129a301bc936fc39f63cdaf830a23
olegsvs <oleg.texet@gmail.com>
2025-08-09 19:10:12 +0300

    Generating icons: Fix transparent bg

--- scripts/generate-icons.sh
+++ scripts/generate-icons.sh
@@ -14,19 +14,19 @@ fi
 mkdir -p public
 
 echo "favicon-16x16.png..."
-convert public/fav.svg -resize 16x16 public/favicon-16x16.png
+convert public/fav.svg -background none -resize 16x16 public/favicon-16x16.png
 
 echo "favicon-32x32.png..."
-convert public/fav.svg -resize 32x32 public/favicon-32x32.png
+convert public/fav.svg -background none -resize 32x32 public/favicon-32x32.png
 
 echo "apple-touch-icon.png..."
-convert public/fav.svg -resize 180x180 public/apple-touch-icon.png
+convert public/fav.svg -background none -resize 180x180 public/apple-touch-icon.png
 
 echo "android-chrome-192x192.png..."
-convert public/fav.svg -resize 192x192 public/android-chrome-192x192.png
+convert public/fav.svg -background none -resize 192x192 public/android-chrome-192x192.png
 
 echo "android-chrome-512x512.png..."
-convert public/fav.svg -resize 512x512 public/android-chrome-512x512.png
+convert public/fav.svg -background none -resize 512x512 public/android-chrome-512x512.png
 
 convert public/fav.svg -resize 1200x630 -background white -gravity center -extent 1200x630 public/og-image.png
 
