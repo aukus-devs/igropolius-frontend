b14fcab11d1ef5b5f38f89b45fcec9154aa4591c
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-08-10 12:45:02 +0200

    Adjust padding in RulesTab component

--- src/components/core/mobile/tabs/RulesTab.tsx
+++ src/components/core/mobile/tabs/RulesTab.tsx
@@ -1,10 +1,10 @@
-import RulesTabs from "@/components/core/rules/RulesTabs";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import RulesTabs from '@/components/core/rules/RulesTabs';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 function RulesTab() {
   return (
-    <ScrollArea className="h-dvh w-full overflow-auto bg-background">
-      <div className="w-full px-4 pt-[110px]">
+    <ScrollArea className="h-dvh w-full overflow-auto bg-background pt-15">
+      <div className="w-full px-4 pt-[40px]">
         <p className="flex flex-col font-wide-black text-[2rem]">
           Правила <span className="text-primary">Игрополиуса</span>
         </p>
@@ -13,7 +13,7 @@ function RulesTab() {
         <RulesTabs />
       </div>
     </ScrollArea>
-  )
+  );
 }
 
 export default RulesTab;
