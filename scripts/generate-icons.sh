6aa9ea17a3f896aa8f3aeee7dc92b4103f4d1252
olegsvs <oleg.texet@gmail.com>
2025-08-10 08:46:20 +0300

    Add 48x48 and 64x64 icons

--- scripts/generate-icons.sh
+++ scripts/generate-icons.sh
@@ -24,6 +24,12 @@ inkscape -w 16 -h 16 public/fav.svg --export-png public/favicon-16x16.png --expo
 echo "favicon-32x32.png..."
 inkscape -w 32 -h 32 public/fav.svg --export-png public/favicon-32x32.png --export-background-opacity=0
 
+echo "favicon-48x48.png..."
+inkscape -w 48 -h 48 public/fav.svg --export-png public/favicon-48x48.png --export-background-opacity=0
+
+echo "favicon-64x64.png..."
+inkscape -w 64 -h 64 public/fav.svg --export-png public/favicon-64x64.png --export-background-opacity=0
+
 echo "favicon-96x96.png..."
 inkscape -w 96 -h 96 public/fav.svg --export-png public/favicon-96x96.png --export-background-opacity=0
 
@@ -38,12 +44,3 @@ inkscape -w 192 -h 192 public/fav.svg --export-png public/android-chrome-192x192
 
 echo "android-chrome-512x512.png..."
 inkscape -w 512 -h 512 public/fav.svg --export-png public/android-chrome-512x512.png --export-background-opacity=0
-
-echo "Done, public/:"
-echo "   - favicon-16x16.png"
-echo "   - favicon-32x32.png"
-echo "   - favicon-96x96.png"
-echo "   - favicon.ico"
-echo "   - apple-touch-icon.png"
-echo "   - android-chrome-192x192.png"
-echo "   - android-chrome-512x512.png"
