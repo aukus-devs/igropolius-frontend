3f625e995c7aecfaa7fba188a619d0b6cc4f963f
olegsvs <oleg.texet@gmail.com>
2025-08-11 07:53:46 +0300

    Update sector names

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -86,7 +86,7 @@ export const sectorsData: SectorData[] = [
   },
   {
     id: 11,
-    name: 'Тюрьма 1',
+    name: 'Тюрьма Домик е*ли',
     position: { x: 10, y: 0 },
     type: 'prison',
     rollType: 'steam',
@@ -166,7 +166,7 @@ export const sectorsData: SectorData[] = [
   },
   {
     id: 21,
-    name: 'Парковка',
+    name: 'Парковка «ну начинается...»',
     position: { x: 10, y: 10 },
     type: 'parking',
     rollType: 'steam',
@@ -246,7 +246,7 @@ export const sectorsData: SectorData[] = [
   },
   {
     id: 31,
-    name: 'Тюрьма 2',
+    name: 'Тюрьма Секреты модератора',
     position: { x: 0, y: 10 },
     type: 'prison',
     rollType: 'steam',
